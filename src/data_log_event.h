8d98e14a9395aaf1327f91831a525f50c87076d7
youmych <youmych@gmail.com>
2018-08-25 23:45:43 +0300

    В Data::Log::Event добавлен метод from_string

--- src/data_log_event.h
+++ src/data_log_event.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cstring>
+
 namespace Data {
 namespace Log {
 
@@ -15,9 +17,25 @@ struct Event {
 
     static const char* to_string(EventType evt) {
         switch(evt) {
-            case EventType::ORDER: return "ORDER";
-            case EventType::UNKNOWN: return "UNKNOWN";
+            case EventType::ORDER:
+                return "ORDER";
+            case EventType::UNKNOWN:
+            default:
+                return "UNKNOWN";
         }
+    }
+
+    static EventType from_string(const char* text, size_t len = 0) {
+        if( !text )
+            return EventType::UNKNOWN;
+
+        if( len == 0 )
+            len = strlen(text);
+
+        if( (len = 5) && (0 == strncmp(text, "ORDER", len)) )
+            return EventType::ORDER;
+        else // if( ... ) else if ( ... ) ...
+            return EventType::UNKNOWN;
     }
 };
 
